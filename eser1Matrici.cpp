74b750bd5760465c9cfe30e0ee6d860adbb7c541
Jacopo <jcp.federico@gmail.com>
2023-03-08 18:19:21 +0100

    upload eser 1 2 3

---
+++ eser1Matrici.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    int righe = 9;
+    int colonne = 9;
+    int matrice[righe][colonne];
+    for (int i = 0; i < righe; i++)
+    {
+        for (int j = 0; j < colonne; j++)
+        {
+            matrice[i][j] = rand() % 10;
+        }
+    }
+    cout << "    ";
+    for (int i = 0; i < colonne; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    int trattini = colonne * 2 + 4;
+    for (int i = 0; i < trattini; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+    for (int i = 0; i < righe; i++)
+    {
+        cout << i << "|  ";
+        for (int j = 0; j < colonne; j++)
+        {
+            cout << matrice[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
